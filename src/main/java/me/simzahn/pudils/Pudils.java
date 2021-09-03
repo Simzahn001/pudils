2812024b2a840157ec6fda71bbd59c34dcfbcd5c
Simzahn001 <hochenberger.simon@ortweinschule.at>
2021-09-03 18:26:47 +0200

    Initial IntelliJ Files
    The basic IntelliJ Files

---
+++ src/main/java/me/simzahn/pudils/Pudils.java
@@ -0,0 +1,17 @@
+package me.simzahn.pudils;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pudils extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
