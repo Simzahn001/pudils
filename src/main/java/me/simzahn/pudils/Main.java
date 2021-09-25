94ab5a399dd18c9be5dc5e731de330f58996a69e
Simzahn001 <hochenberger.simon@ortweinschule.at>
2021-09-25 18:07:44 +0200

    Timer Class
    Created the Timer
    Started with the Timer command

--- src/main/java/me/simzahn/pudils/Main.java
+++ src/main/java/me/simzahn/pudils/Main.java
@@ -1,10 +1,15 @@
 package me.simzahn.pudils;
 
 import com.zaxxer.hikari.HikariDataSource;
+import me.simzahn.pudils.timer.Timer;
+import me.simzahn.pudils.timer.TimerCom;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.sql.Time;
 
 public final class Main extends JavaPlugin {
 
+    private static Timer timer;
     private static Main plugin;
     private HikariDataSource hikari;
 
@@ -12,6 +17,7 @@ public final class Main extends JavaPlugin {
     public void onEnable() {
         //Main Singleton
         plugin = this;
+        timer = new Timer();
 
         //HikariCP Setup
         hikari = new HikariDataSource();
@@ -22,6 +28,8 @@ public final class Main extends JavaPlugin {
         hikari.addDataSourceProperty("user", this.getConfig().get("db.user"));
         hikari.addDataSourceProperty("password", this.getConfig().get("db.password"));
 
+
+        getCommand("timer").setExecutor(new TimerCom());
     }
 
     @Override
@@ -39,5 +47,9 @@ public final class Main extends JavaPlugin {
 
     public HikariDataSource getHikari() {
         return hikari;
+    }
+
+    public static Timer getTimer() {
+        return timer;
     }
 }
---
+++ src/main/java/me/simzahn/pudils/timer/Timer.java
@@ -0,0 +1,82 @@
+package me.simzahn.pudils.timer;
+
+import me.simzahn.pudils.Main;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
+
+public class Timer {
+
+    private int seconds;
+    private boolean isRunning = false;
+    private BukkitTask runnable;
+
+    public Timer() {
+        this.seconds = Main.getPlugin().getConfig().getInt("timer.seconds");
+    }
+
+    public void start() {
+        reset();
+        resume();
+    }
+
+    public void resume() {
+        if(this.isRunning) {
+            Bukkit.getOnlinePlayers().forEach( player -> player.sendMessage("§4§fDer Timer konnte nicht gestartet werden, weil er bereits läuft!"));
+        }else {
+            this.runnable = new BukkitRunnable() {
+                @Override
+                public void run() {
+                    //just for safety
+                    if(!isRunning) {
+                        stop();
+                    }
+
+                    for(Player currentPlayer : Bukkit.getOnlinePlayers()) {
+                        currentPlayer.sendActionBar("§6§l" + ((int)seconds/60/60<10?"0"+(int)seconds/60/60:(int)seconds/60/60) + ":" + (seconds/60%60<10?"0"+seconds/60%60:seconds/60%60) + ":" + (seconds%60<10?"0"+seconds%60:seconds%60));
+                    }
+                    seconds++;
+                }
+            }.runTaskTimer(Main.getPlugin(), 20, 20);
+        }
+    }
+
+    public void stop() {
+        if(!this.isRunning) {
+            Bukkit.getOnlinePlayers().forEach( player -> player.sendMessage("§4§fDer Timer konnte nicht gestoppt werden, weil er bereit gestoppt wurde!") );
+        }else {
+            for (Player currentPlayer :  Bukkit.getOnlinePlayers()) {
+                currentPlayer.sendActionBar("§4§fDer Timer wurde gestoppt!");
+            }
+            Main.getPlugin().getConfig().set("timer.seconds", seconds);
+            Main.getPlugin().saveConfig();
+            this.isRunning = false;
+            runnable.cancel();
+        }
+    }
+
+    public void reset() {
+        stop();
+        Main.getPlugin().getConfig().set("timer.seconds", 0);
+        Main.getPlugin().saveConfig();
+    }
+
+
+
+
+
+
+
+
+
+    //Getter & Setter
+
+    public int getSeconds() {
+        return seconds;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+}
