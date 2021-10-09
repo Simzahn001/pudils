03c10179af85dc0f24bd79278a2ce236db6c47b3
Simzahn001 <hochenberger.simon@ortweinschule.at>
2021-10-09 15:22:13 +0200

    Team Timer check
    Checked trough the Team when the Timer is started

--- src/main/java/me/simzahn/pudils/timer/Timer.java
+++ src/main/java/me/simzahn/pudils/timer/Timer.java
@@ -2,15 +2,22 @@ package me.simzahn.pudils.timer;
 
 import me.simzahn.pudils.Main;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class Timer {
 
     private int seconds;
     private boolean isRunning = false;
     private BukkitTask runnable;
+    private String SELECT = "SELECT uuid,playing FROM player";
 
     public Timer() {
         this.seconds = Main.getPlugin().getConfig().getInt("timer.seconds");
@@ -25,6 +32,31 @@ public class Timer {
         if(this.isRunning) {
             Bukkit.getOnlinePlayers().forEach( player -> player.sendMessage("§4§fDer Timer konnte nicht gestartet werden, weil er bereits läuft!"));
         }else {
+            //check trough the team
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    try(Connection connection = Main.getPlugin().getHikari().getConnection();
+                        PreparedStatement select = connection.prepareStatement(SELECT)) {
+
+                        ResultSet result = select.executeQuery();
+
+                        while(result.next()) {
+                            if(Bukkit.getPlayer(result.getString("uuid")).isOnline()) {
+                                if (result.getBoolean("playing")) {
+                                    Bukkit.getPlayer(result.getString("uuid")).setGameMode(GameMode.SURVIVAL);
+                                }else {
+                                    Bukkit.getPlayer(result.getString("uuid")).setGameMode(GameMode.CREATIVE);
+                                }
+                            }
+                        }
+
+                    } catch (SQLException exception) {
+                        exception.printStackTrace();
+                    }
+                }
+            }.runTaskAsynchronously(Main.getPlugin());
+
             isRunning = true;
             this.runnable = new BukkitRunnable() {
                 @Override
